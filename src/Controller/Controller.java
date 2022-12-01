05b65deb5d56cee1e18f22d5842f2e86cc24b13c
Ashkan Z <ashkan.zeinolabedini@mail.utoronto.ca>
2022-12-01 13:51:15 -0500

    Initial an Shownimage object and imageview sets the image view

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -5,12 +5,14 @@ import View.MainView;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import Controller.ShownImage;
 
 public class Controller {
     ThemeControl themeCont;
     Stage stage;
     Scene scene;
     ImageView imageV;
+    ShownImage picture;
 
     MainView mView;
     public Controller(Stage stage){
@@ -18,9 +20,13 @@ public class Controller {
         this.themeCont = new ThemeControl();
         this.mView = new MainView(this);
 
+        this.picture = new ShownImage();
+        imageV = picture.getImgView();
+        mView.setCurrImage(imageV);
 
         mView.initUI();
         scene = new Scene(mView.getbPane(), 950, 650);
+
 
         this.stage.setScene(scene);
         this.stage.show();
