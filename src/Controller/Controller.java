b1459fce6eea712a2fc41880e9842c7cd14a790b
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-14 00:47:40 -0500

    Refactored to move view responsibilities out of the controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,76 +1,39 @@
 package Controller;
 
 import Colours.ThemeControl;
-import View.MainView;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-import Controller.ShownImage;
 
 public class Controller {
     ThemeControl themeCont;
-    Stage stage;
 
-    double sceneWidth = 950;
-    double sceneHeight = 650;
-    Scene scene;
-    ImageView imageV;
     ShownImage picture;
+    String imgPath = System.getProperty("user.dir") + "\\TestImages\\DSC_4236_01.jpg";
 
-    MainView mView;
-    public Controller(Stage stage){
-        this.stage = stage;
-        this.themeCont = new ThemeControl();
 
-        this.picture = new ShownImage();
-        this.picture.setController(this);
-        this.picture.updateHeight(sceneHeight);
-        this.picture.updateWidth(sceneWidth);
-
-        imageV = picture.getImgView();
-
-        mView = new MainView(this);
-        mView.setCurrImage(imageV);
-        mView.initUI();
-
-        scene = new Scene(mView.getbPane(), sceneWidth, sceneHeight);
-
-        // got following listeners from: https://blog.idrsolutions.com/2012/11/adding-a-window-resize-listener-to-javafx-scene/
-        scene.widthProperty().addListener(new ChangeListener<Number>() {
-            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
-                setSceneWidth();
-
-                imageV = picture.getImgView();
-                mView.setCurrImage(imageV);
-            }
-        });
-        scene.heightProperty().addListener(new ChangeListener<Number>() {
-            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
-                setSceneHeight();
-
-                imageV = picture.getImgView();
-                mView.setCurrImage(imageV);
-            }
-        });
-
-        this.stage.setScene(scene);
-        this.stage.show();
+    public Controller(){
+        this.picture = new ShownImage(imgPath);
+        themeCont = new ThemeControl();
     }
 
     public ThemeControl getThemeCont(){
         return this.themeCont;
     }
 
-    public void setSceneWidth(){
-        sceneWidth = scene.getWidth();
-
-        this.picture.updateWidth(sceneWidth);
+    public void setSceneWidth(double width){
+        this.picture.updateWidth(width);
     }
-    public void setSceneHeight(){
-        sceneHeight = scene.getHeight();
+    public void setSceneHeight(double height){
+        this.picture.updateHeight(height);
+    }
 
-        this.picture.updateHeight(sceneHeight);
+    public ImageView getImage(double width, double height){
+        this.picture.updateHeight(height);
+        this.picture.updateWidth(width);
+
+        return picture.getImgView();
+    }
+
+    public ImageView getImage(){
+        return picture.getImgView();
     }
 }
--- src/View/MainView.java
+++ src/View/MainView.java
@@ -2,31 +2,66 @@ package View;
 
 import Colours.ThemeControl;
 import Controller.Controller;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import javafx.scene.image.ImageView;
 
 public class MainView {
-    ImageView currImage;
     BorderPane bPane;
     ThemeControl themeCont;
     Controller control;
+    Stage stage;
+    Scene scene;
 
-    public MainView(Controller control) {
-        this.control = control;
+    ImageView imgV;
+
+    public MainView(Stage stage) {
+        this.stage = stage;
+        control = new Controller();
+
+        imgV = control.getImage(950,650);
 
         themeCont = control.getThemeCont();
+
+
+
+        initUI();
+
     }
-    public void initUI(){ //maybe have starting image file path passed in here
+    private void initUI(){ //maybe have starting image file path passed in here
         bPane = new BorderPane();
         bPane.setStyle(themeCont.getBackColour());
 
-        bPane.setCenter(currImage);
+        bPane.setCenter(imgV);
+
+
+        scene = new Scene(bPane, 950, 650);
+        // got following listeners from: https://blog.idrsolutions.com/2012/11/adding-a-window-resize-listener-to-javafx-scene/
+        scene.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                control.setSceneWidth(scene.getWidth());
+
+                imgV = control.getImage();
+            }
+        });
+        scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                control.setSceneHeight(scene.getHeight());
+
+                imgV = control.getImage();
+            }
+        });
+
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void setCurrImage(ImageView imgV){
-        this.currImage = imgV;
+    public void setImage(ImageView imgV){
+        this.imgV = imgV;
     }
     public BorderPane getbPane(){
         return this.bPane;
